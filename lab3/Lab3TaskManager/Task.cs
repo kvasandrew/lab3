d4b08ab7c5cd554b9d4eb280cc79faf281946cfc
Andrii <myemail@gmail.com>
2023-12-20 14:43:50 +0200

    Start of the project

---
+++ lab3/Lab3TaskManager/Task.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3TaskManager
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public bool Favorite { get; set; }
+        public Task(string title,string description, string priority, bool favorite) 
+        { 
+            Title = title;
+            Description = description;
+            Priority = priority;
+            Favorite = favorite;
+        }
+
+            
+        public Task()
+        {
+
+            Title = "Noname";
+            Description = "Nothing";
+            Priority = null;
+            Favorite = false;
+        }
+  
+
+    }
+}
---
+++ lab3/Lab3TaskManager/TaskManagement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3TaskManager
+{
+    public class TaskManagement
+    {
+        public List<Task> Tasks { get; }
+
+        public TaskManagement() { Tasks = new List<Task>(); }
+        public bool CheckPriority(string priority)
+        {
+            switch (priority)
+            {
+                case null:
+                case "Lowest":
+                case "Low":
+                case "Medium":
+                case "High":
+                case "Highest":
+                    return true;
+                default: return false;
+            }
+        }
+        public bool IsValid(Task task)
+        {
+            if (string.IsNullOrWhiteSpace(task.Title) || string.IsNullOrWhiteSpace(task.Description) || !CheckPriority(task.Priority)) return false; return true;
+        }
+        private bool TaskExist(string title){return Tasks.Where(x => x.Title == title).Any();}
+       
+
+        
+    }
+}
